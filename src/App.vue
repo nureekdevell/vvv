9f70eeeead91fb4391ba3d54995c76a63dba3d99
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-02-29 23:36:45 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<RouterLink to="/">Home</RouterLink>
+	<RouterLink to="/about">About</RouterLink>
+	<RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style lang="scss" scoped></style>
