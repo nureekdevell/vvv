7a30a774643b5872a356656b7a9ba59259c6add4
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-03-01 22:42:37 +0500

    axios added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
-	<div>Home</div>
+	<div>
+		<ul>
+			<li v-for="project of projects" :key="project.id">
+				<h2>{{ project.id }}</h2>
+				<h2>{{ project.title }}</h2>
+				<h2>{{ project.deadline }}</h2>
+			</li>
+		</ul>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+let projects = ref([]);
+
+onMounted(async () => {
+	let response = await axios.get('http://localhost:3000/projects');
+	projects.value = response.data;
+	// let respone = await fetch('http://localhost:3000/projects');
+	// let data = await respone.json();
+	// for (let element of data) {
+	// 	console.log(element);
+	// }
+});
+</script>
 
 <style lang="scss" scoped></style>
